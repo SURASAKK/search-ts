a15aba325dde507b3eb08f694ad3dda88cdfc676
SURASAKK <surasak.bcircle@gmail.com>
2018-07-03 12:12:03 +0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from 'axios';
 
 
 type State = {
@@ -11,18 +12,47 @@ class App extends React.Component<{}, State> {
     constructor(props) {
         super(props);
         this.state = {
-            path: "/Users/wk",
+            path: "/Users/surasak/Project/HelloWorld/src/SearchApi",
             pattern: "*.json"
+            
         }
     }
 
     onClick = (e) => {
-        console.log("Hello, world!");
+        console.log(this.state)
+     
+ 
+        axios.post('http://localhost:5000/api/search/searchFile', {
+        path: this.state.path, 
+        pattern: this.state.pattern,
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': 'application/json'
+      
+      }
+    
+      })
+      .then((response) => {
+        console.log(response.data)
+
+    })
+    .catch((error) => {
+        console.log("AXIOS ERROR: ", error)
+    })
+    
     }
 
     render() {
         return (
-            <h1 onClick={this.onClick}>Hello, world!</h1>
+            <div>
+            <h1>Search File</h1>
+            <div>
+                <button onClick={this.onClick} >Search</button>
+              
+
+
+            </div>   
+        </div>       
         )
     }
 }
